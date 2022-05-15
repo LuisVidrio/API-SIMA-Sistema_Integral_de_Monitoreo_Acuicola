d6383c747ef888ee0b97d489dd5ff9eafd361bab
cesar angulo <cesarangulo@cesars-MacBook-Air-2.local>
2022-05-15 11:40:35 -0600

    SIMA-7 migracion de BD Monitoreo

--- Helpers/DataContext.cs
+++ Helpers/DataContext.cs
@@ -6,6 +6,12 @@ using WebApi.Entities;
 public class DataContext : DbContext
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<Pond> Ponds { get; set; }
+    public DbSet<IOT_Module> IOT_Modules { get; set; }
+
+    public DbSet<IOT_Device> IOT_Devices { get; set; }
+    public DbSet<IOT_Value> IOT_Values { get; set; }
+
 
     private readonly IConfiguration Configuration;
 
@@ -19,4 +25,15 @@ public class DataContext : DbContext
        var connectionString = Configuration.GetConnectionString("SIMA");
         options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
     }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<IOT_Value>()
+           .Property(c => c.created_at)
+           .HasColumnType("datetime(3)")
+           .IsRequired();
+        modelBuilder.Entity<IOT_Value>()
+            .HasKey(c => new { c.IOT_DeviceId, c.created_at });
+    }
+
 }
---
+++ Migrations/20220515143121_InitialCreate.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Ponds",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Altitude = table.Column<int>(type: "int", nullable: false),
+                    Longitud = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ponds", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LastName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Username = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Role = table.Column<int>(type: "int", nullable: false),
+                    PasswordHash = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "IOT_Modules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PondId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IOT_Modules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IOT_Modules_Ponds_PondId",
+                        column: x => x.PondId,
+                        principalTable: "Ponds",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "IOT_Devices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    IOT_ModuleId = table.Column<int>(type: "int", nullable: false),
+                    Parameter = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IOT_Devices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IOT_Devices_IOT_Modules_IOT_ModuleId",
+                        column: x => x.IOT_ModuleId,
+                        principalTable: "IOT_Modules",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "IOT_Values",
+                columns: table => new
+                {
+                    IOT_DeviceId = table.Column<int>(type: "int", nullable: false),
+                    created_at = table.Column<DateTime>(type: "datetime(3)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IOT_Values", x => new { x.IOT_DeviceId, x.created_at });
+                    table.ForeignKey(
+                        name: "FK_IOT_Values_IOT_Devices_IOT_DeviceId",
+                        column: x => x.IOT_DeviceId,
+                        principalTable: "IOT_Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IOT_Devices_IOT_ModuleId",
+                table: "IOT_Devices",
+                column: "IOT_ModuleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IOT_Modules_PondId",
+                table: "IOT_Modules",
+                column: "PondId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IOT_Values");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "IOT_Devices");
+
+            migrationBuilder.DropTable(
+                name: "IOT_Modules");
+
+            migrationBuilder.DropTable(
+                name: "Ponds");
+        }
+    }
+}
